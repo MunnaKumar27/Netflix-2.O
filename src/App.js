70577f0d1d1e6743d2e54d5b2f3ab2b28cf43817
MunnaKumar27 <mkumar270278@gmail.com>
2022-12-20 17:12:03 +0530

    hmm acchha thik h

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
